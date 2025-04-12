450cad056ce518e0da408ef350012b92b9b2b88c
fishxet <ggfhappinesss@gmail.com>
2025-04-13 01:35:15 +0300

    clearing master for sub-branch

--- cs12dotnet8/Chapter02/AboutSize/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿public class Program {
-    public static void Main(string[] args) {
-        Console.BackgroundColor = ConsoleColor.DarkMagenta;
-        Console.WriteLine(Environment.UserDomainName);
-    }
-}
